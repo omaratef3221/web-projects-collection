16152cd2477b8977e45b8bc29a1e2bd4a63ff95a
Omar Atef <omaratef3221@gmail.com>
2023-07-16 00:55:25 +0400

    first commit

---
+++ functions.js
@@ -0,0 +1,7 @@
+function change_page(project_name){
+    if (project_name = "calculator"){
+        document.getElementById("calculator_button").onclick = function() {
+            window.location.href = '/calculator.html';
+                }
+        }
+    };
